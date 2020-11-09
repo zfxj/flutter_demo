130e6a9553fc704ca12a81c3a2a6c3c1de333839
jun <xujun_work@163.com>
2020-11-09 09:50:11 +0800

    test

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
-# flutterdemo
+1、flutter doctor:检测flutter
+2、flutter emulator
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+3、flutter emulator --launch Pixel_2_API_30  :唤起对应的模拟器
+4、flutter run :运行项目
--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,10 @@ class App extends StatelessWidget {
     return MaterialApp(
       // debugShowCheckedModeBanner: false,
       home: Home(),
-      theme: ThemeData(primarySwatch: Colors.yellow),
+      theme: ThemeData(
+          primarySwatch: Colors.yellow,
+          highlightColor: Color.fromRGBO(255, 255, 255, 0.5), // 点击tab主体高亮颜色
+          splashColor: Colors.white70), //点击tab 水波纹的颜色
     );
   }
 }
@@ -19,17 +22,68 @@ class App extends StatelessWidget {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.grey[100],
-        appBar: AppBar(
-          title: Text('xujun'),
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Navagation',
-            onPressed: null,
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+          backgroundColor: Colors.grey[100],
+          appBar: AppBar(
+            title: Text('xujun'),
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              tooltip: 'Navagation',
+              onPressed: () => debugPrint('flutter 测8试'),
+            ),
+            elevation: 80.0, // 标题栏下方的的阴影
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                tooltip: 'Navagation',
+                onPressed: () => debugPrint('flutter 搜索'),
+              ),
+            ],
+            bottom: TabBar(
+              unselectedLabelColor: Colors.black38, // 未被选中的标签样式
+              indicatorColor: Colors.black54, //横线样式
+              indicatorSize: TabBarIndicatorSize.label, // 横线和label一样长
+              indicatorWeight: 3.0, // 横线粗细
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.local_florist)),
+                Tab(icon: Icon(Icons.local_airport)),
+                Tab(icon: Icon(Icons.local_atm))
+              ],
+            ),
           ),
-          elevation: 80.0, // 标题栏下方的的阴影
-        ),
-        body: null);
+          body: TabBarView(
+            // tab内容区 需要和tabs 里的数量一致
+            children: <Widget>[
+              Icon(
+                Icons.local_florist,
+                size: 180,
+                color: Colors.black12,
+              ),
+              Icon(
+                Icons.local_airport,
+                size: 180,
+                color: Colors.black12,
+              ),
+              Icon(
+                Icons.local_atm,
+                size: 180,
+                color: Colors.black12,
+              ),
+            ],
+          ),
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero, // 边距为零
+              children: <Widget>[
+                DrawerHeader(
+                  child: Text('header'.toUpperCase()),
+                  decoration: BoxDecoration(color: Colors.grey[100]),
+                )
+              ],
+            ),
+          )),
+    );
   }
 }
